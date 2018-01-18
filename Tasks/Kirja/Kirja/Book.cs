95ad27831810903c8dc2e9e159b4f44ed700d543
jniculescu <jani.niculescu@gmail.com>
2018-01-18 09:29:51 +0200

    Task Kirja redi.

---
+++ Tasks/Kirja/Kirja/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kirja
+{
+    class Book
+    {
+        private string _author;
+        private string _title;        
+        private int _id;
+        private int _price;
+
+        public Book()
+        {
+            this._author = "Joku Jokutin";
+            this._title = "Kirja 1";
+            this._id = 1;
+            this._price = 20;                            
+        }
+
+        public Book(string author, string title, int id, int price)
+        {
+            this._author = author;
+            this._title = title;
+            this._id = id;
+            this._price = price;
+        }
+
+        public void Bookinfo()
+        {
+            Console.WriteLine("Kirja: " + _title + ", Tekijä: " + _author + ", tunniste: " + _id + ", hinta: " + _price);
+        }
+
+        public string CompareBook(Book book)
+        {
+            if(this._price > book._price)
+            {
+                return $"{this._title} on kalliimpi kuin {book._title}";
+            }
+
+            else
+            {
+                return $"{this._title} on halvempi kuin {book._title}";
+            }
+        }
+    }
+}
---
+++ Tasks/Kirja/Kirja/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kirja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Tohelo", "kirja2", 2, 15);
+            Book book2 = new Book("joku jokutin", "kirja3", 3, 11);
+
+            book.Bookinfo();
+            book2.Bookinfo();
+
+            Console.WriteLine(book.CompareBook(book2));
+
+            Console.ReadKey();
+        }
+    }
+}
