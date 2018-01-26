cbcf89c63f9963c4aa9e15f28cbf124d7cb893b7
jniculescu <jani.niculescu@gmail.com>
2018-01-26 14:30:29 +0200

    auto task done

---
+++ Tasks/Auto/Auto/Auto/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    class Car
+    {
+        private string _brand;
+        private double _speed;
+                    
+        public Car()
+        {
+            _brand = "unknown";
+            _speed = 0;
+        }
+
+        public Car(string brand, double speed)
+        {
+            _brand = brand;
+            _speed = speed;
+
+        }
+                                
+        public void AskData()
+        {
+            Console.WriteLine("Give brand and speed of vehicle.");
+            Console.WriteLine("Brand: ");
+            _brand = Console.ReadLine();
+            Console.WriteLine("Speed: ");
+            double.TryParse(Console.ReadLine(), out _speed);  
+        }
+
+        public void ShowCarinfo()
+        {
+            Console.WriteLine($"Car was a {_brand} with a speed of {_speed}.");
+        }
+
+        public void Accelerate(int number)
+        {
+            _speed = _speed + number;
+        }
+
+        public void Brake(int number)
+        {
+            _speed = (_speed * number) / 100;
+        }
+    }
+}
---
+++ Tasks/Auto/Auto/Auto/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car car1 = new Car();
+
+            Car carAuto1 = new Car();
+            car1.AskData();
+            car1.ShowCarinfo();
+            Console.WriteLine("Car speed after 30+:");
+            car1.Accelerate(30);
+            car1.ShowCarinfo();
+            Console.WriteLine("Car speed after 10% slowdown:");
+            car1.Brake(90);
+            car1.ShowCarinfo();
+
+            Car car2 = new Car();
+            car2.AskData();
+            car2.ShowCarinfo();
+            Console.WriteLine("Car speed after 30+.");
+            car2.Accelerate(30);
+            car2.ShowCarinfo();
+            Console.WriteLine("Car speed after 10% slowdown.");
+            car2.Brake(90);
+            car2.ShowCarinfo();
+
+            Console.ReadKey();
+        }
+    }
+}
