5ce070d8ae21a066c79e81b67e51e4faa5121b07
jniculescu <jani.niculescu@gmail.com>
2018-01-26 14:59:07 +0200

    firma task done

---
+++ Tasks/Firma/Firma/Company.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firma
+{
+    class Company
+    {
+        private string _title;
+        private string _address;
+        private int _phone;
+        private int _income;
+        private int _expenses;
+
+        public Company()
+        {
+            _title = "company";
+            _address = "somewhere";
+            _phone = 0;
+            _income = 0;
+            _expenses = 0;
+        }
+
+        public Company(string title, string address, int phone, int income, int expenses)
+        {
+            _title = title;
+            _address = address;
+            _phone = phone;
+            _income = income;
+            _expenses = expenses;
+        }
+
+        public Company(Company previousCompany)
+        {
+            _title = previousCompany._title;
+            _address = previousCompany._address;
+            _phone = previousCompany._phone;
+            _income = previousCompany._income;
+            _expenses = previousCompany._expenses;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Title = {_title}\nAddress = {_address}\nPhone number = {_phone}\nOutcome = {_income}\nExpence = {_expenses}\n");
+        }
+
+        public void ShowProfit()
+        {
+            int profit = (_income - _expenses) / _expenses * 100;
+
+            if (profit < 2 * _expenses)
+            {
+                Console.WriteLine($"\n{_title}: Company's profits are under 100 % bigger than expences. Company is not doing so great.");
+            }
+
+            else if (profit <= 3 * _expenses)
+            {
+                Console.WriteLine($"\n{_title}: Company's profits are at least 200 % bigger than expences. Company is doing fine.");
+            }
+
+            else
+            {
+                Console.WriteLine($"\n{_title}: Company's profits are at least 300 % bigger than expences. Company is doing very well!");
+            }
+        }
+    }
+}
---
+++ Tasks/Firma/Firma/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Firma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company company1 = new Company();
+            company1._address = "somewhere 123";
+            company1._title = "the nimi";
+            company1._phone = 040096534;
+            company1._income = 900000;
+            company1._expenses = 778654;
+            Company company2 = new Company(company1);
+
+            Company company3 = new Company("comp3", "comp 3", 04006723, 1006777, 467568);
+                        
+            company1.ShowInfo();
+            company2.ShowInfo();
+            company2.ShowProfit();
+            company3.ShowProfit();
+
+            Console.ReadKey();
+        }
+    }
+}
