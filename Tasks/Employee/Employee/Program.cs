35462f5e75daf879a3bc0034d946f3d869381c9c
jniculescu <jani.niculescu@gmail.com>
2018-01-26 14:39:33 +0200

    changes

--- Tasks/Employee/Employee/Program.cs
+++ Tasks/Employee/Employee/Program.cs
@@ -11,9 +11,9 @@ namespace Employee
         static void Main(string[] args)
         {
             Employee[] employees = new Employee[3];
-            employees[0] = new Employee("Amanda", 123456, "Manager", 4900);
-            employees[1] = new Employee("Henna", 234567, "Programmer", 5500);
-            employees[2] = new Employee("Tia", 345678, "Programmer", 6000);
+            employees[0] = new Employee("Mira", 981123, "Manager", 8600);
+            employees[1] = new Employee("Pete", 451235, "Programmer", 6442);
+            employees[2] = new Employee("Jani", 956743, "Programmer", 4355);
 
             employees[0].PrintEmployeeInfo();
             employees[1].PrintEmployeeInfo();
