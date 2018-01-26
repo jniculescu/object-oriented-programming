45277b211340361859f94aefbe5fdf20cc0fcdc9
jniculescu <jani.niculescu@gmail.com>
2018-01-26 14:37:38 +0200

    employee done

---
+++ Tasks/Employee/Employee/Employee.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Employee
+    {
+        public string Name;
+        public int Id;
+        public string Position;
+        public decimal Salary;
+
+        public Employee()
+        {
+            Name = "joku";
+            Id = 0;
+            Position = "jossain";
+            Salary = 0.0m;
+        }
+
+        public Employee(string name, int id, string position, decimal salary)
+        {
+            Name = name;
+            Id = id;
+            Position = position;
+            Salary = salary;
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"Name = {Name}\nId = {Id}\nPosition = {Position}\nSalary = {Salary:f2}\n");
+        }
+
+        public void CompareSalary(Employee worker)
+        {
+            if (Salary > worker.Salary)
+            {
+                Console.WriteLine($"{Name} has bigger salary than {worker.Name} - {Salary - worker.Salary}\n");
+            }
+            else
+            {
+                Console.WriteLine($"{worker.Name} has bigger salary than {Name} - {worker.Salary - Salary}\n");
+            }
+        }
+    }
+}
---
+++ Tasks/Employee/Employee/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("Amanda", 123456, "Manager", 4900);
+            employees[1] = new Employee("Henna", 234567, "Programmer", 5500);
+            employees[2] = new Employee("Tia", 345678, "Programmer", 6000);
+
+            employees[0].PrintEmployeeInfo();
+            employees[1].PrintEmployeeInfo();
+            employees[2].PrintEmployeeInfo();
+
+            employees[0].CompareSalary(employees[1]);
+            employees[0].CompareSalary(employees[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
