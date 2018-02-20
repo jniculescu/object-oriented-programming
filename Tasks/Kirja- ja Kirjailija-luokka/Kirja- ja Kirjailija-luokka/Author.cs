299266389cdec82a915829db015259011fc84123
jniculescu <jani.niculescu@gmail.com>
2018-02-20 16:10:08 +0200

    Done

---
+++ Tasks/Kirja- ja Kirjailija-luokka/Kirja- ja Kirjailija-luokka/Author.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kirja__ja_Kirjailija_luokka
+{
+    class Author
+    {
+        public string Name;
+        public string Birthday;
+        public string Book;
+
+        public Author()
+        {
+            Name = "unknown";
+            Birthday = "unknown";
+            Book = "unknown";
+        }
+
+        public Author(string name, string birthday, string book)
+        {
+            Name = name;
+            Birthday = birthday;
+            Book = book;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine($"\nAuthors name: {Name}\nBirthday: {Birthday}\nBooks name: {Book}");
+        }
+    }
+}
--- Tasks/Kirja- ja Kirjailija-luokka/Kirja- ja Kirjailija-luokka/Program.cs
+++ Tasks/Kirja- ja Kirjailija-luokka/Kirja- ja Kirjailija-luokka/Program.cs
@@ -11,18 +11,17 @@ namespace Kirja__ja_Kirjailija_luokka
         static void Main(string[] args)
         {
             Book book1 = new Book("The Hunt For Red October", "Tom Clancy", "WSOY", 25, "War");
-            Book book2 = new Book("", "Emily Bronte", "Bookpublishers", 24.60, "Romance");
-            Book book3 = new Book("To Kill a Mockingbird", "Lee Harper", "Bookpublishers", 32.40, "Drama");
-            Book book4 = new Book("The Lord of the Rings", "J.R.R. Tolkien", "Allen & Unwin", 35.00, "Fantasy");
+            Book book2 = new Book("The Lord of the Rings", "J.R.R. Tolkien", "Allen & Unwin", 40.00, "Fantasy");
 
             book1.SearchBook("The Hunt For Red October");
-            book2.SearchBook("Wuthering Heights");
+            book2.SearchBook("The Lord of the Rings");
 
             Console.WriteLine($"Category now: {book1.themeName}");
-            book1.SwitchTheme("Drama");
+            book1.SwitchTheme("Fantasy");
             Console.WriteLine($"Category now: {book1.themeName}");
+            Author author = new Author("Tom Clancy", "12.4.1947", "The Hunt For Red October");
 
-            Console.ReadKey();
+            Console.ReadKey();                        
         }
     }
 }
